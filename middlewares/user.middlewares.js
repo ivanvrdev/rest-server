cc08f712246268d4f6d6c6a921b074b638fe1edc
ivanvrdev <ivangaston20032017@gmail.com>
2021-09-24 16:49:12 -0300

    middlewares

--- middlewares/user.middlewares.js
+++ middlewares/user.middlewares.js
@@ -1,8 +1,10 @@
 const {body} = require('express-validator');
+const {validateFields} = require('./validation');
 
 const userValidation = [
     body('username', 'El email ingresado no es válido').isEmail(),
-    body('password', 'La contraseña debe contener entre 8 y 20 caracteres').isLength({min:8, max:20})
+    body('password', 'La contraseña debe contener entre 8 y 20 caracteres').isLength({min:8, max:20}),
+    validateFields
 ];
 
 module.exports = {userValidation};
--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,11 +1,10 @@
 const router = require('express').Router();
 const {allUsers, newUser, editUser, routePUTDelete, routeDELETE} = require('../controllers/users.controllers');
 const {userValidation} = require('../middlewares/user.middlewares');
-const {validateFields} = require('../helpers/validation');
 
 //Rutas
 router.get('/users/all', allUsers);
-router.post('/users/new',userValidation, validateFields, newUser);
+router.post('/users/new',userValidation, newUser);
 router.put('/users/edit', editUser);
 router.put('/users/delete', routePUTDelete); //Eliminación lógica
 router.delete('/users/delete', routeDELETE); //Eliminación física

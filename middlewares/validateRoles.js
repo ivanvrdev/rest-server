2347f947a54cbe166d4500dee9034d32da736e55
gastonVR <ivangaston20032017@gmail.com>
2021-10-01 08:10:45 -0300

    documentación

--- middlewares/validateRoles.js
+++ middlewares/validateRoles.js
@@ -1,5 +1,11 @@
 const {request} = require('express')
 
+/**
+ * Es un middleware custom que compara el rol del usuario que realiza
+ *  una petición con los roles especeficados al llamar la función
+ * @param  {...string} roles 
+ * @returns respuesta negativa o deja pasar la request al siguente middleware.
+ */
 const validateRoles = (...roles) =>{
     return (req = request, res, next) =>{
         if(!roles.includes(req.user.role)){
